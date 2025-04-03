1bf1602910c13aaed660044af0ba12eb49688c36
VictorAnizauBarros <victorhugo.AnizauEstudos@outlook.com>
2025-04-02 22:22:50 -0300

    first commit

---
+++ config/db.js
@@ -0,0 +1,18 @@
+const mysql = require('mysql');
+
+const conexao = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'crud_mvc'
+});
+
+conexao.connect((erro)=>{
+    if(erro){
+        console.error('Erro ao conectar ao banco de dados:', erro);
+        return;
+    }
+    console.log('Conexão com o banco de dados estabelecida!')
+});
+
+module.exports = conexao;
